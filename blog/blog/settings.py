c3f8f3623aaf4aa5875e56f3f0ab655cd6b9125a
hgzhangzhen <hgzhangzhen@126.com>
2025-01-24 11:57:47 +0800

    'createdatabase';

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'Aa123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'ATOMIC_REQUESTS': True
     }
 }
 
